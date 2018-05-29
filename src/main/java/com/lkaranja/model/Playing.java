90aab376ef01bcf700e81ddedc7376651081b23a
Lawrence <lawrenslava1@gmail.com>
2018-05-29 19:46:50 +0300

    Add class to fetch currently playing movies in theatres

---
+++ src/main/java/com/lkaranja/model/Playing.java
@@ -0,0 +1,65 @@
+
+package com.lkaranja;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Playing {
+
+    private List<Result> results = null;
+    private Integer page;
+    private Integer totalResults;
+    private Dates dates;
+    private Integer totalPages;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public List<Result> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Result> results) {
+        this.results = results;
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public Dates getDates() {
+        return dates;
+    }
+
+    public void setDates(Dates dates) {
+        this.dates = dates;
+    }
+
+    public Integer getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
