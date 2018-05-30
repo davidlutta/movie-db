59ece5eb5b768bcca486ecbb07b06432a0af7487
Lawrence <lawrenslava1@gmail.com>
2018-05-30 19:42:03 +0300

    Add parameters for fetching currently playing movies

--- src/main/java/com/lkaranja/Constants.java
+++ src/main/java/com/lkaranja/Constants.java
@@ -10,9 +10,9 @@ public class Constants {
     public static final String SORT = "popularity.desc";
 
 
-    public static final String PLAYING_PARAMETER = "true";
-    public static final String PLAYING = "now_playing";
-    
+    public static final String PLAYING_URL_PARAMETER = "https://api.themoviedb.org/3/movie/now_playing";
+    // public static final String PLAYING = "now_playing";
+
 
     public static final String API_PARAMETER = "api_key";
     public static final String API = "7e71e907dd76aa2aea6fe16ba270239a";
